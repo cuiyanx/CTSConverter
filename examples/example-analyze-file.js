0a8d40aa136cdd115fa993c941c04919bbd462a5
Cui Yan <yanx.cui@intel.com>
2018-06-06 10:47:44 +0800

    Examples: analyze python file
    Signed-off-by: Cui Yan <yanx.cui@intel.com>

--- examples/example-analyze-file.js
+++ examples/example-analyze-file.js
@@ -2,9 +2,9 @@ const File_Analyzer = require("../lib/analyze-file-to-lines.js");
 
 File_Analyzer.init_array();
 
-var filepath = "./tmp/specs/V1_0/add.mod.py";
+var filepath = "./tmp/specs/V1_0/mul_relu.mod.py";
 var line_array = File_Analyzer.analyzer(filepath);
 
-for (let x = 0; x < line_array.length; x++) {
+for (let x in line_array) {
     console.log(line_array[x]);
 }
