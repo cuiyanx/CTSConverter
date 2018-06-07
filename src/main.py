c7d3a222535bff9b927e1ae18b3931065c3e6b1f
Cui Yan <yanx.cui@intel.com>
2018-06-07 17:31:25 +0800

    Change the idea of construction
    Signed-off-by: Cui Yan <yanx.cui@intel.com>

---
+++ src/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+name_file = "./filename.txt"
+
+with open(name_file) as file_names:
+  print "Open names file: " + name_file
+
+  lines = file_names.readlines()
+
+input_path_parent = "./src/nn/runtime/test/specs/"
+output_path_parent = "./output/"
+
+versions = os.listdir(input_path_parent)
+
+for version in versions:
+  input_path = input_path_parent + version + "/"
+  output_path = output_path_parent + version + "/"
+
+  if os.path.isdir(input_path):
+    if not os.path.exists(output_path):
+      os.makedirs(output_path)
+
+    for line in lines:
+      line = line.strip()
+
+      output_path_example = output_path + "examples/"
+
+      if not os.path.exists(output_path_example):
+        os.makedirs(output_path_example)
+
+      output_path_model = output_path + "models/"
+
+      if not os.path.exists(output_path_model):
+        os.makedirs(output_path_model)
+
+      input_file = input_path + line
+      output_file_example = output_path_example + line[:-6] + "example.cpp"
+      output_file_model = output_path_model + line[:-6] + "model.cpp"
+
+      if os.path.exists(input_file):
+        cmd = "python3 ./src/test_generator.py -m " + output_file_model + " -e " + output_file_example + " " + input_file
+        os.system(cmd);
