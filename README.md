e466ff837e2a0a3be7d15dd13d6b9835d0512fc9
cuiyan <yanx.cui@intel.com>
2019-03-27 15:09:28 +0800

    Update command code
    Signed-off-by: cuiyan <yanx.cui@intel.com>

--- README.md
+++ README.md
@@ -8,30 +8,41 @@ Transfer [nn](https://android.googlesource.com/platform/frameworks/ml) test case
 
 ### create `cts-all.js` into `./output`, this file include nn and plus test case
 
-```sh
+```shell
 $ npm start
-```
-
-### transfer nn test cases and create `nn-all.js` into `./output`
-
-```sh
-$ npm run nn
-```
-
-### scan supplement directory and create `supplement-all.js` into `./output`
-
-```sh
-$ npm run supplement
-```
-
-### scan plus directory and create `plus-all.js` into `./output`
-
-```sh
-$ npm run plus
 ```
 
 ### get more information for command line
 
-```sh
+```shell
 $ npm run info
+```
+
+```shell
+-h, --help            show this help message and exit
+-a, --all             [option] '-a [file name]', create all test file
+-t, --transfer        [option] '-t [transfer directory]', transfer nn test file
+-c, --cts             [option] '-c [cts directory]', include cts test file
+-s, --supplement      [option] '-s [supplement directory]', include supplement test file
+-p, --plus            [option] '-p [plus directory]', include plus test file
+```
+
+## Example
+
+1. Transfer nn test cases from `./src/nn` and output into `./output/cts`.
+
+```shell
+$ python3 ./src/main.py -t ./src/nn
+```
+
+2. Transfer nn test cases from `./src/nn` and create all test case file as `all.js`.
+
+```shell
+$ python3 ./src/main.py -t ./src/nn -c ./output/cts -a all.js
+```
+
+3. Create all test case file as `all-cts-supplement-plus.js` that include cts and supplement and plus test case.
+
+```shell
+$ python3 ./src/main.py -c ./output/cts -s ./test_supplement -p ./V1_0_plus -a all-cts-supplement-plus.js
 ```
--- src/main.py
+++ src/main.py
@@ -5,14 +5,15 @@ import argparse
 
 def get_args():
   parser = argparse.ArgumentParser()
-  parser.add_argument("-c", "--create", help = "'-c [file name]', create all test file", default = "-")
-  parser.add_argument("-t", "--transfer", help = "[option] '-t [nn directory]', transfer and include nn test file", default = "-")
+  parser.add_argument("-a", "--all", help = "[option] '-a [file name]', create all test file", default = "-")
+  parser.add_argument("-t", "--transfer", help = "[option] '-t [transfer directory]', transfer nn test file", default = "-")
+  parser.add_argument("-c", "--cts", help = "[option] '-c [cts directory]', include cts test file", default = "-")
   parser.add_argument("-s", "--supplement", help = "[option] '-s [supplement directory]', include supplement test file", default = "-")
   parser.add_argument("-p", "--plus", help = "[option] '-p [plus directory]', include plus test file", default = "-")
 
   args = parser.parse_args()
 
-  return (args.create, args.transfer, args.supplement, args.plus)
+  return (args.all, args.transfer, args.cts, args.supplement, args.plus)
 
 def del_file(path, flag):
   names = os.listdir(path)
@@ -26,41 +27,10 @@ def del_file(path, flag):
       os.remove(file_path)
 
   if flag:
-    os.remove(path)
-
-# transfer nn test case to js test case
-def transfer(ipath, opath, names):
-  transfer_file_dict = dict()
-
-  versions = os.listdir(ipath)
-
-  for version in versions:
-    input_path = os.path.join(ipath, version)
-    output_path = os.path.join(opath, version)
-
-    if os.path.exists(output_path):
-      del_file(output_path, False)
-    else:
-      os.makedirs(output_path)
-
-    if os.path.isdir(input_path):
-      for name in names:
-        name = name.strip()
-
-        input_file = os.path.join(input_path, name)
-        output_file = os.path.join(output_path, name[:-6] + "js")
-
-        if os.path.exists(input_file):
-          transfer_file_dict[name[:-6] + "js"] = output_file
-
-          cmd = "python3 ./src/test_generator.py " + input_file +\
-                " -js " + output_file
-          os.system(cmd)
-
-  return transfer_file_dict
+    os.rmdir(path)
 
 # scan test case directory
-def get_file_names(ipath):
+def get_file_names(ipath, suffixName):
   file_names_dict = dict()
 
   names = os.listdir(ipath)
@@ -69,14 +39,35 @@ def get_file_names(ipath):
     path_or_file = os.path.join(ipath, name)
 
     if os.path.isfile(path_or_file):
-      if name[-3:] == ".js":
+      if name[-3:] == suffixName:
         file_names_dict[name] = path_or_file
-#        print ("include: " + path_or_file)
     else:
-      tmp_dict = get_file_names(path_or_file)
+      tmp_dict = get_file_names(path_or_file, suffixName)
       file_names_dict.update(tmp_dict)
 
   return file_names_dict
+
+# transfer nn test case to js test case
+def transfer(ipath, opath, names):
+  dirs = os.listdir(ipath)
+
+  for dir in dirs:
+    idir = os.path.join(ipath, dir)
+    odir = os.path.join(opath, dir)
+
+    if os.path.isdir(idir):
+      os.makedirs(odir)
+      transfer(idir, odir, names)
+    else:
+      for name in names:
+        name = name.strip()
+
+        if name == dir:
+          input_file = idir
+          output_file = os.path.join(opath, dir[:-6] + "js")
+          cmd = "python3 ./src/test_generator.py " + input_file +\
+                " -js " + output_file
+          os.system(cmd)
 
 # create all test case file
 def create(opath, file_dict, file_list):
@@ -103,17 +94,23 @@ def create(opath, file_dict, file_list):
     all_jsTest_file.write("});\n")
 
 if __name__ == "__main__":
-  (args_create, args_transfer, args_supplement, args_plus) = get_args()
+  (args_all, args_transfer, args_cts, args_supplement, args_plus) = get_args()
 
   output_path_root = "./output"
   if not os.path.exists(output_path_root):
     os.makedirs(output_path_root)
 
-  output_file_all = os.path.join(output_path_root, args_create)
+  output_file_all = os.path.join(output_path_root, args_all)
 
-  file_dict = dict()
+  file_dict_all = dict()
 
   if not args_transfer == "-":
+    output_path_transfer = os.path.join(output_path_root, "cts")
+    if os.path.exists(output_path_transfer):
+      del_file(output_path_transfer, False)
+    else:
+      os.makedirs(output_path_transfer)
+
     support_cts_file = "./slice.txt"
 
     with open(support_cts_file) as cts_file:
@@ -121,24 +118,28 @@ if __name__ == "__main__":
       print ("Open support cts file: " + support_cts_file + "\n")
 
     print ("transfer nn test case to js test case....\n")
-    transfer_file_dict = transfer(args_transfer, output_path_root, cts_file_names)
-    file_dict.update(transfer_file_dict)
+    transfer(args_transfer, output_path_transfer, cts_file_names)
+
+  if not args_cts == "-":
+    print ("scan test cts directory....\n")
+    cts_file_dict = get_file_names(args_cts, ".js")
+    file_dict_all.update(cts_file_dict)
 
   if not args_supplement == "-":
     print ("scan test supplement directory....\n")
-    supplement_file_dict = get_file_names(args_supplement)
-    file_dict.update(supplement_file_dict)
+    supplement_file_dict = get_file_names(args_supplement, ".js")
+    file_dict_all.update(supplement_file_dict)
 
   if not args_plus == "-":
     print ("scan test plus directory....\n")
-    plus_file_dict = get_file_names(args_plus)
-    file_dict.update(plus_file_dict)
+    plus_file_dict = get_file_names(args_plus, ".js")
+    file_dict_all.update(plus_file_dict)
 
-  print ("reordering by name....\n")
-  file_list = sorted(file_dict.keys())
+  if not args_all == "-":
+    print ("reordering by name....\n")
+    file_list = sorted(file_dict_all.keys())
 
-  if not args_create == "-":
     print ("create all test case file....\n")
-    create(output_file_all, file_dict, file_list)
+    create(output_file_all, file_dict_all, file_list)
 
   print ("transfer and create all files are completed")
